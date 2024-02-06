dadde81bba8f0ef7ab70f5631723d702b9b481f7
Zicheng Liu <z.liu116@lse.ac.uk>
2024-02-06 04:06:24 +0000

    update radar plots

--- radar_plots.r
+++ radar_plots.r
@@ -5,14 +5,37 @@ library(ggradar)
 df <- read_csv("./data/top10cuisines_nutrients_normalised.csv")
 print(df)
 
+for (i in colnames(df)[2:ncol(df)]) {
+    selected_df <- 
+        df %>%
+        select(c("cuisine", i)) %>%
+        ggradar(
+            plot.title = paste("Nutrient Profile of", i, "Cuisine"),
+            font.radar = 'roboto',
+            values.radar = c(0, 5, 10),
+            grid.label.size = 5,
+            axis.label.size = 5, # Afftects the names of the variables
+            group.point.size = 2,   # Simply the size of the points
+            gridline.min.linetype = 2,
+            gridline.mid.linetype = 1,
+            gridline.max.linetype = 1,
+        ) %>%
+        ggsave(filename = paste0("./plots/", i, "_radar_plot.png"), width = 16, height = 9, units = "in", dpi = 600)
+}
 # Create a radar plot
-plot <- df %>%
-    ggradar( 
-    font.radar = 'roboto',
-    grid.label.size = 1,
-    axis.label.size = 5, # Afftects the names of the variables
-    group.point.size = 1   # Simply the size of the point
-    )
+# plot <- df %>%
+#     ggradar(
+#     plot.title = "Nutrient Profiles of Top 10 Most Popular Cuisines",
+#     font.radar = 'roboto',
+#     legend.title = "Cuisine",
+#     values.radar = c(0, 5, 10),
+#     grid.label.size = 5,
+#     axis.label.size = 5, # Afftects the names of the variables
+#     group.point.size = 2,   # Simply the size of the points
+#     gridline.min.linetype = 2,
+#     gridline.mid.linetype = 1,
+#     gridline.max.linetype = 1,
+#     )
 
-# Save the plot
-ggsave(plot, filename = "./plots/nutrients_radar_plot.png", width = 16, height = 9, units = "in", dpi = 600)
+# # Save the plot
+# ggsave(plot, filename = "./plots/nutrients_radar_plot.png", width = 16, height = 9, units = "in", dpi = 600)
