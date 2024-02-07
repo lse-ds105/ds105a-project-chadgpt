5b8bc78f780162a4f95a2b9964d852b6606b3883
Zicheng Liu <z.liu116@lse.ac.uk>
2024-02-07 04:14:28 +0000

    resize radar plots

--- scripts/radar_plots.r
+++ scripts/radar_plots.r
@@ -12,12 +12,13 @@ for (i in df$cuisine) {
        arrange(cuisine != " healthy threshold")
    plot <- selected_df %>%
        ggradar(
-           group.colours = c("#BFBFBF", "#5C3DA4"),
+           group.colours = c("#858585", "#5C3DA4"),
+           base.size = 8,
            plot.title = paste0("Nutrient Profile of ", i, " Cuisine"),
            font.radar = 'roboto',
            values.radar = c(0, 5, 10),
            grid.label.size = 5,
-           axis.label.size = 5, # Affects the names of the variables
+           axis.label.size = 6, # Affects the names of the variables
            group.line.width = 1,
            group.point.size = 2,   # size of the the points
            gridline.min.linetype = 1,
@@ -25,6 +26,6 @@ for (i in df$cuisine) {
            gridline.max.linetype = 1,
            legend.position = "top"
        ) %>%
-       ggsave(filename = paste0("../docs/plots/radar_plot_", i, ".png"), width = 10, height = 10, units = "in", dpi = 600)
+       ggsave(filename = paste0("../docs/plots/radar_plot_", i, ".png"), width = 8, height = 8, units = "in", dpi = 600)
     print(paste0("../docs/plots/radar/radar_plot_", i, ".png"))
 }
